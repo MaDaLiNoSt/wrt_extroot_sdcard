bcb1590952a23b29bec47404040d947ae99b348a
MADALIN <7978656+MaDaLiNoSt@users.noreply.github.com>
2024-05-02 14:06:41 +0300

    test

---
+++ README.md
@@ -0,0 +1,7 @@
+# wrt_extroot_sdcard
+
+
+
+```sh
+wget https://raw.githubusercontent.com/MaDaLiNoSt/wrt_extroot_sdcard/main/sh.sh -O - | sh
+```
---
+++ sh.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+opkg update
+opkg install block-mount kmod-fs-ext4 e2fsprogs parted kmod-usb-storage
+
+mkfs.ext4 /dev/sda1
+
+DISK="/dev/mmcblk"
+yes y |parted -s ${DISK} -- mklabel gpt mkpart extroot 2048s -2048s
+DEVICE="${DISK}0"
+yes y |mkfs.ext4 -L extroot ${DEVICE}
+
+eval $(block info ${DEVICE} | grep -o -e 'UUID="\S*"')
+eval $(block info | grep -o -e 'MOUNT="\S*/overlay"')
+uci -q delete fstab.extroot
+uci set fstab.extroot="mount"
+uci set fstab.extroot.uuid="${UUID}"
+uci set fstab.extroot.target="${MOUNT}"
+uci commit fstab
+
+ORIG="$(block info | sed -n -e '/MOUNT="\S*\/overlay"/s/:\s.*$//p')"
+uci -q delete fstab.rwm
+uci set fstab.rwm="mount"
+uci set fstab.rwm.device="${ORIG}"
+uci set fstab.rwm.target="/rwm"
+uci commit fstab
+
+mount ${DEVICE} /mnt
+tar -C ${MOUNT} -cvf - . | tar -C /mnt -xf -
+
+reboot
